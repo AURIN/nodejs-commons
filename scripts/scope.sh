832d171ca1ccd62a6ccb9fa3b08f276bac4ef2b3
Christopher Bayliss <christopher.bayliss@unimelb.edu.au>
2020-10-02 15:03:26 +1000

    Fixes npx trying to install `json` globally and failing
    Replaces `npx json` with just `json` as npm adds `node_modules/.bin/` to script paths by default

--- scripts/scope.sh
+++ scripts/scope.sh
@@ -7,7 +7,7 @@ scope() {
     echo "Package name already scoped"
   else
     echo "Package name not scoped"
-    npx json -f package.json -e "this.name=\"@AURIN/${npm_package_name}\""
+    json -I -f package.json -e "this.name=\"@AURIN/${npm_package_name}\""
   fi
 }
 
